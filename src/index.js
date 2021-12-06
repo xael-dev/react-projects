5d9a4938afc90ec412cc696a4cb45820fe6e6f2a
alexanderJHarris <ajharris8814@gmail.com>
2021-12-06 18:11:30 -0500

    Add test code to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import Navbar from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Test header</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
