b185f82963479b2b4d2364db654ae9d65c869dd6
alexanderJHarris <ajharris8814@gmail.com>
2021-12-06 23:58:34 -0500

    Add navlink for home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,9 @@ const Navbar = () => {
             <Nav>
                 <Bars />
                 <NavMenu>
+                    <NavLink to='/' activeStyle>
+                        Home
+                    </NavLink>
                     <NavLink to='/about' activeStyle>
                         About
                     </NavLink>
