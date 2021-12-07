0890aa20eee28aed2e6f799b38bac4db8982aaed
alexanderJHarris <ajharris8814@gmail.com>
2021-12-06 22:30:22 -0500

    Setup routes for navbar links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/index';
+import { About } from './pages/about';
+import { Blogs } from './pages/blog';
+import { SignUp } from './pages/signup';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/blog' element={<Blogs />} />
+                <Route path='/signup' element={<SignUp />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
