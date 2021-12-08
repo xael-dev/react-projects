bc2465e5d9458eaa59f2123ce43e6a78bc87c93c
alexanderJHarris <ajharris8814@gmail.com>
2021-12-07 21:09:56 -0500

    Adjust header placement

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 export const SignUp = () => {
     return (
+        //define JSX element styles as an object
         <div style={{
             display: 'flex',
-            justifyContent: 'Right',
-            alignItems: 'Right',
+            justifyContent: 'Center',
             height: '100vh'
         }}>
             <h1>Sign Up</h1>
         </div>
+        //There should be a form here
     )
 }
